5061d34ddc32a4507ca8fe99632507a4fc05ef20
Shivendra Raghuvanshi <developer.raghuvanshi@gmail.com>
2025-05-26 19:05:01 +0530

    Fixed the MongoDB transport uri

--- initialize/logging.js
+++ initialize/logging.js
@@ -32,7 +32,7 @@ const logger = winston.createLogger({
 logger.add(
   new winston.transports.MongoDB({
     level: "error",
-    db: "mongodb://127.0.0.1:27017/FareWheels",
+    db: process.env.MONGO_URI,
   })
 );
 
